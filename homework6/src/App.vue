90fd8a553dbc75924184ff6208a4f64559896506
Vladimir <maostyle@mail.ru>
2023-12-07 21:30:40 +0300

    completed homework6

---
+++ homework6/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="main">
+    <HeaderComponent/>,
+    <!-- <MainPage />, -->
+    <!-- <BlogPosts />, -->
+    <!-- <BlogDetails /> -->
+    <ProjectPage/>,
+    <FooterComponent/>,
+  </div>
+</template>
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+// import MainPage from './components/MainPage.vue'
+// import BlogPosts from './components/BlogPosts.vue'
+// import BlogDetails from './components/BlogDetails.vue'
+import ProjectPage from './components/ProjectPage.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    // MainPage,
+    // BlogPosts,
+    // BlogDetails,
+    ProjectPage,
+    FooterComponent
+  }
+}
+</script>
+<style lang="scss">
+@import "B:/study/frontend/JavaScript/VUEjs/homework4/src/assets/styles/style.scss";
+</style>
---
+++ homework6/src/components/ProjectItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="main-project__projects__categories__category-item">
+      <img
+        :src="require(`B:/study/frontend/JavaScript/VUEjs/homework6/src/assets/images/${itemData.image}`)"
+        alt=""
+        class="main-project__projects__categories__category-item-image"
+      />
+        <div class="main-project__projects__categories__category-item-info">
+        <div class="main-project__projects__categories__category-item__info-left">
+          <div class="main-project__projects__categories__category-item title">{{ itemData.itemTitle }}</div>
+          <div class="main-project__projects__categories__category-item text">{{ itemData.itemText }}</div>
+        </div>
+        <div class="main-project__projects__categories__category-item
+        __info-right">
+        <img src="../assets/images/arrow.svg" alt="arrow">
+        </div>
+      </div>
+    </div>
+  </template>
+<script>
+export default {
+  name: 'ProjectItem',
+  props: ['itemData', 'itemID', 'Category'],
+
+  data () {
+    return {}
+  }
+}
+</script>
