a7142a4c113f8c3db210835ffc22f59cc89caa25
Vladimir <maostyle@mail.ru>
2023-12-10 16:46:44 +0300

    seminair7

---
+++ seminair7/src/components/AddForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input v-model="name"  type="text">
+        <input v-model.number ="price" type="nimber">
+        <button @click="addToCart">ADD</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AddForm',
+
+  data () {
+    return {
+      name: '',
+      price: null
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+    addToCart () {
+      const id = new Date().getTime()
+      const newProduct = {
+        id, name: this.name, price: this.price
+      }
+      this.$emit('addToCart', newProduct)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
